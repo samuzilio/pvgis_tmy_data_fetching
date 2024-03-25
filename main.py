ca7ac6710d4efbb543e8e7b207b09f9686fcd6cb
samuzilio <94171193+samuzilio@users.noreply.github.com>
2024-03-25 17:40:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from tqdm import tqdm
+from preprocessing import VARIABLE_MAP, get_pvgis_tmy_data
+
+def main():
+    # Read point data from the CSV file
+    df = pd.read_csv("sample_point_data.csv")
+
+    # Prompt the user to select a variable for calculation
+    while True:
+        selected_variable = input(
+            "Enter the variable you want to calculate the mean for (e.g., ghi, dni, dhi, etc.): "
+        )
+        if selected_variable in VARIABLE_MAP.values():
+            break
+        else:
+            print("Invalid variable name! Please try again.")
+
+    # Process point data for each row (i.e., calculate mean variable values for given set of coordinates)
+    for row in tqdm(
+        df.itertuples(index=False),
+        total=len(df),
+        desc="Processing locations",
+    ):
+        identifier, latitude, longitude = row.id, row.latitude, row.longitude
+        data, months_selected, inputs, meta = get_pvgis_tmy_data(
+            latitude, longitude, startyear=2010, endyear=2020
+        )
+        variable_values = data[selected_variable]
+        mean_variable_values = variable_values.mean()
+        df.loc[df["id"] == identifier, f"mean_{selected_variable}"] = (
+            mean_variable_values
+        )
+
+    # Store the result in a new CSV file
+    df.to_csv(f"sample_point_data_with_mean_{selected_variable}.csv", index=False)
+
+if __name__ == "__main__":
+    main()
---
+++ preprocessing.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import requests
+import io
+
+# Provide API entry point for PVGIS 5.2
+URL = "https://re.jrc.ec.europa.eu/api/v5_2/"
+
+# Rename PVGIS TMY variables to more descriptive names
+VARIABLE_MAP = {
+    "G(h)": "ghi",
+    "Gb(n)": "dni",
+    "Gd(h)": "dhi",
+    "G(i)": "poa_global",
+    "Gb(i)": "poa_direct",
+    "Gd(i)": "poa_sky_diffuse",
+    "Gr(i)": "poa_ground_diffuse",
+    "H_sun": "solar_elevation",
+    "T2m": "temp_air",
+    "RH": "relative_humidity",
+    "SP": "pressure",
+    "WS10m": "wind_speed",
+    "WD10m": "wind_direction",
+}
+
+# Function to get PVGIS TMY data
+def get_pvgis_tmy_data(
+    latitude,
+    longitude,
+    outputformat="csv",
+    usehorizon=True,
+    userhorizon=None,
+    startyear=None,
+    endyear=None,
+    map_variables=True,
+    url=URL,
+    timeout=30,
+):
+    params = {"lat": latitude, "lon": longitude, "outputformat": outputformat}
+    params["usehorizon"] = int(usehorizon)
+    params["userhorizon"] = (
+        ",".join(map(str, userhorizon)) if userhorizon is not None else None
+    )
+    params["startyear"] = startyear
+    params["endyear"] = endyear
+    res = requests.get(url + "tmy", params=params, timeout=timeout)
+    res.raise_for_status()
+    data, months_selected, inputs, meta = None, None, None, None
+    if outputformat == "csv":
+        with io.BytesIO(res.content) as src:
+            data, months_selected, inputs, meta = _parse_pvgis_tmy_data(src)
+    if map_variables:
+        data = data.rename(columns=VARIABLE_MAP)
+    return data, months_selected, inputs, meta
+
+# Function to parse fetched PVGIS TMY data
+def _parse_pvgis_tmy_data(src):
+    inputs = {}
+    inputs["latitude"] = float(src.readline().split(b":")[1])
+    inputs["longitude"] = float(src.readline().split(b":")[1])
+    inputs["elevation"] = float(src.readline().split(b":")[1])
+    src.readline()
+    months_selected = [
+        {"month": month + 1, "year": int(src.readline().split(b",")[1])}
+        for month in range(12)
+    ]
+    headers = [h.decode("utf-8").strip() for h in src.readline().split(b",")]
+    data = pd.DataFrame(
+        [src.readline().split(b",") for _ in range(8760)], columns=headers
+    )
+    dtidx = pd.to_datetime(
+        data["time(UTC)"].apply(lambda dt: dt.decode("utf-8")),
+        format="%Y%m%d:%H%M",
+        utc=True,
+    )
+    data = data.drop("time(UTC)", axis=1)
+    data = pd.DataFrame(data, dtype=float)
+    data.index = dtidx
+    meta = [line.decode("utf-8").strip() for line in src.readlines()]
+    return data, months_selected, inputs, meta
